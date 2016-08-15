ff4e33aa2729ff0e33e565e1d5acab64556c5c5e
Valdir Stumm Junior <stummjr@gmail.com>
2016-08-15 17:06:15 -0300

    first import

---
+++ demo/settings.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for demo project
+
+BOT_NAME = 'demo'
+
+SPIDER_MODULES = ['demo.spiders']
+NEWSPIDER_MODULE = 'demo.spiders'
+
+
+ROBOTSTXT_OBEY = True
